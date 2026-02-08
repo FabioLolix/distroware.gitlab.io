e0f25c323a85d8cab5b4ef4539024920d47ef2ad
FabioLolix <fabio.loli@disroot.org>
2026-02-09 00:27:33 +0100

    Update cp.sh

---
+++ cp.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+cp docs/contributing/fullfilling.md CONTRIBUTING.md
+cp docs/index.md README.md
+
+# Workaround for GitLab issue https://gitlab.com/gitlab-org/gitlab/-/issues/214066
+# Support README.md being a symbolic link
