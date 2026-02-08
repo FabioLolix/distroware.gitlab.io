f2885bfc1288fbbe04ed001ae885ba7bf1f7f9c7
FabioLolix <fabio.loli@disroot.org>
2026-02-09 00:45:15 +0100

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -5,3 +5,4 @@ cp docs/index.md README.md
 
 # Workaround for GitLab issue https://gitlab.com/gitlab-org/gitlab/-/issues/214066
 # Support README.md being a symbolic link
+# As reported in https://gitlab.com/Distroware/distroware.gitlab.io/-/issues/29
